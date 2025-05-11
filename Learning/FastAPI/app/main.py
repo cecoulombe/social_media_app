f13f512710fa28d3b3dec85e526fda481e383734
Caitlin Coulombe <ccoulomb@student.ubc.ca>
2025-05-11 11:13:28 -0600

    Changed deprecated .dict() for used .model_dump()

--- Learning/FastAPI/app/main.py
+++ Learning/FastAPI/app/main.py
@@ -45,7 +45,7 @@ def get_posts():
 # create a path operation to create a post
 @app.post("/posts", status_code=status.HTTP_201_CREATED)    # the status code will change to the correct status code whenever the create is done right
 def create_post(post: Post):    # passing the class above will mean it so that the api automatically validates the input received based on the class described above
-    post_dict = post.dict()
+    post_dict = post.model_dump()
     post_dict['id'] = randrange(0, 10000000)
     my_posts.append(post_dict)
     return {"data:": post_dict}
@@ -83,7 +83,7 @@ def update_post(id: int, post: Post):
     if index == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"post with id: {id} was not found") 
-    post_dict = post.dict()
+    post_dict = post.model_dump()
     post_dict["id"] = id
     my_posts[index] = post_dict
     return {"data": post_dict}
