e2a8e7a1bc7323d00f71143b40e0bc80e1f4f0ad
Caitlin Coulombe <ccoulomb@student.ubc.ca>
2025-05-12 23:33:12 -0600

    Added utils.py which contains the function for hashing passwords. Implemented password hashing when storing user passwords. Added ability to get a single user based on id. Added a schema so that returned users never show passwords.

--- Learning/FastAPI/app/main.py
+++ Learning/FastAPI/app/main.py
@@ -4,6 +4,7 @@ from random import randrange
 import psycopg2;
 from psycopg2.extras import RealDictCursor;
 from app import schema as sch;
+from app import utils;
 
 # Create a FastAPI application
 app = FastAPI()
@@ -75,8 +76,29 @@ def update_post(id: int, post: sch.Post):
 # ----------------------- USERS LOGIC -----------------------
 # Create a new user
 @app.post("/users", status_code=status.HTTP_201_CREATED)
-def create_user(user: sch.CreateUser):
-    cursor.execute("""INSERT INTO users (email, password) VALUES (%s, %s) RETURNING *""", (user.email, user.password))
-    new_user = cursor.fetchone()
-    conn.commit()  
-    return {"data": new_user}
+def create_user(user: sch.UserCreate):
+    try:
+        # Hash the password - user.password
+        hashed_password = utils.hash(user.password)
+        user.password = hashed_password
+
+        # Adding the pydantic model of the user to the table
+        cursor.execute("""INSERT INTO users (email, password) VALUES (%s, %s) RETURNING *""", (user.email, user.password))
+        new_user = cursor.fetchone()
+        conn.commit()  
+        return {"data": sch.UserOut(**new_user)}
+    except psycopg2.errors.UniqueViolation:
+        conn.rollback()
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Email already registered")
+    except Exception as e:
+        conn.rollback()
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail = f"Unexpected error: {str(e)}")
+    
+# Retreive the information from a specific user based on id
+@app.get("/users/{id}")
+def get_user(id: int):
+    cursor.execute("""SELECT * FROM users WHERE id = %s""", (str(id),))
+    user = cursor.fetchone()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"User with id: {id} does not exist")
+    return {"data": sch.UserOut(**user)}
--- Learning/FastAPI/app/schema.py
+++ Learning/FastAPI/app/schema.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from pydantic import BaseModel, EmailStr
 
 # ----------------------- SCHEMA -----------------------
@@ -7,7 +8,13 @@ class Post(BaseModel):  #this will expand the basemodel from pydantic
     content: str
     published: bool = True     # this gives a default value if the user doesn't enter a value (makes it optional)
 
-# schema used to manage user data
-class CreateUser(BaseModel):
+# schema used to create user data
+class UserCreate(BaseModel):
     email: EmailStr
     password: str
+
+#schema used to return user data (don't ever want to send back the password)
+class UserOut(BaseModel):
+    id: int
+    email: EmailStr
+    created_at: datetime
