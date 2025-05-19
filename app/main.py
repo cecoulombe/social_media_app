2af32229c6d99cd17a2bc95e09041f0acebd2832
Caitlin Coulombe <ccoulomb@student.ubc.ca>
2025-05-19 14:39:38 -0600

    Added CORSmiddleware to determine which domains can and cannot access the api. Will change later to limit allowed domains to the domain of the site.

--- app/main.py
+++ app/main.py
@@ -1,9 +1,21 @@
 from fastapi import Body, FastAPI, Response, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from app.routers import post, user, auth, like
 from app.config import settings
 
 # Create a FastAPI application
 app = FastAPI()
+
+# determines which domains can access the api (currently set to all but will change to the website domain when published)
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(post.router)
 app.include_router(user.router)
